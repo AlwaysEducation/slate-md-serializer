5db3ab9bdb8a50648ba0f5a368473f4f6e2e82eb
Andrew Lively <2755389+andrewlively@users.noreply.github.com>
2019-08-26 14:45:21 -0400

    Don't overescape URLs

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
+function isURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
+
 export function escapeMarkdownChars(text: string): string {
   let result = text;
 
@@ -9,6 +19,11 @@ export function escapeMarkdownChars(text: string): string {
 
   // Hashtags shouldn't be escaped, but elsewhere should
   result = result.replace(/(#\s)/gi, "\\$1");
+  
+  // Check if the result is a URL, if so then return here so we don't overescape
+  if (isURL(result)) {
+    return result;
+  }
 
   // Catch all escaping for certain characters
   // TODO: situationally escape these characters so we don't overescape
